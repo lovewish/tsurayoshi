1daead102f78b380cbcb8fe6e9334dc377e56c64
FJ2-0288\Owner <aaa>
2024-05-28 14:48:40 +0900

    4 commit

--- src/main/java/com/example/demo/form/ChatController.java
+++ src/main/java/com/example/demo/form/ChatController.java
@@ -1,7 +1,13 @@
 package com.example.demo.form;
+import java.io.File;
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class ChatController {
 
@@ -15,5 +21,26 @@ public class ChatController {
 	public String sample2(Model model){
 		return "body";
 	}
+	
+    @GetMapping("/random-image")
+    @ResponseBody
+    public String getRandomImage() {
+        File directory = new File("static/image");
+        File[] files = directory.listFiles();
+        if (files != null && files.length > 0) {
+            Random random = new Random();
+            File randomFile = files[random.nextInt(files.length)];
+            return "/image/" + randomFile.getName();
+        }
+        return "random";
+    }
+	
+    
+    @GetMapping("/random")
+    public String show1() {
+        return "random";
+    }
+    
+    
 }
 	
